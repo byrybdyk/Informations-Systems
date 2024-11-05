f6619c074b55a8d1cb28b35dd3678927b7616944
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2024-11-05 14:22:10 +0300

    refactor: createLabWork

--- backend/src/main/java/com/byrybdyk/lb1/service/LabWorkService.java
+++ backend/src/main/java/com/byrybdyk/lb1/service/LabWorkService.java
@@ -1,6 +1,9 @@
 package com.byrybdyk.lb1.service;
 
+import com.byrybdyk.lb1.dto.LabWorkDTO;
+import com.byrybdyk.lb1.model.Discipline;
 import com.byrybdyk.lb1.model.LabWork;
+import com.byrybdyk.lb1.model.Person;
 import com.byrybdyk.lb1.repository.LabWorkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +14,15 @@ import java.util.List;
 public class LabWorkService {
 
     private final LabWorkRepository labWorkRepository;
+    private final DisciplineService disciplineService;
+    private final PersonService authorService;
 
     @Autowired
-    public LabWorkService(LabWorkRepository labWorkRepository) {
+    public LabWorkService(LabWorkRepository labWorkRepository, DisciplineService disciplineService, PersonService authorService) {
         this.labWorkRepository = labWorkRepository;
+        this.disciplineService = disciplineService;
+        this.authorService = authorService;
+
     }
 
     public LabWork saveLabWork(LabWork labWork) {
@@ -23,6 +31,32 @@ public class LabWorkService {
 
     public List<LabWork> getAllLabWorks() {
         return labWorkRepository.findAll();
+    }
+
+    private void mapDtoToLabWork(LabWork labWork, LabWorkDTO labWorkDTO, Person author) {
+        labWork.setName(labWorkDTO.getName());
+        labWork.setDescription(labWorkDTO.getDescription());
+        labWork.setDifficulty(labWorkDTO.getDifficulty());
+        labWork.setCoordinates(labWorkDTO.getCoordinates());
+        labWork.setMinimalPoint(labWorkDTO.getMinimalPoint());
+        labWork.setPersonalQualitiesMinimum(labWorkDTO.getPersonalQualitiesMinimum());
+        labWork.setPersonalQualitiesMaximum(labWorkDTO.getPersonalQualitiesMaximum());
+        labWork.setAuthor(author);
+    }
+
+    public LabWork createLabWorkFromDTO(LabWorkDTO labWorkDTO) {
+        LabWork labWork = new LabWork();
+
+        Person author = authorService.getOrCreateAuthor(labWorkDTO.getAuthorId(), labWorkDTO.getAuthor());
+        labWork.setAuthor(author);
+
+        Discipline discipline = disciplineService.getOrCreateDiscipline(labWorkDTO.getDiscipline());
+        labWork.setDiscipline(discipline);
+
+        mapDtoToLabWork(labWork, labWorkDTO, author);
+
+        LabWork createdLabWork = saveLabWork(labWork);
+        return createdLabWork;
     }
 
 }
