1dce6515e086485f9d51f51c50127b4d18ed9f2a
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2024-11-13 03:09:46 +0300

    feat: add admin's history page

--- backend/src/main/java/com/byrybdyk/lb1/controller/AdminController.java
+++ backend/src/main/java/com/byrybdyk/lb1/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.byrybdyk.lb1.controller;
 
 import com.byrybdyk.lb1.model.AdminRequest;
+import com.byrybdyk.lb1.model.LabWorkHistory;
 import com.byrybdyk.lb1.service.AdminRequestService;
+import com.byrybdyk.lb1.service.LabWorkHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +20,12 @@ import java.util.List;
 public class AdminController {
 
     private final AdminRequestService adminRequestService;
+    private final LabWorkHistoryService labWorkHistoryService;
 
     @Autowired
-    public AdminController(AdminRequestService adminRequestService) {
+    public AdminController(AdminRequestService adminRequestService, LabWorkHistoryService labWorkHistoryService) {
         this.adminRequestService = adminRequestService;
+        this.labWorkHistoryService = labWorkHistoryService;
     }
 
     @GetMapping("/requests")
@@ -28,6 +33,15 @@ public class AdminController {
         List<AdminRequest> requests = adminRequestService.getAllRequests();
         model.addAttribute("requests", requests);
         return "admin-requests";
+    }
+
+    @GetMapping("/history")
+    public String viewHistory(Model model, Authentication authentication) {
+        List<LabWorkHistory> historyList = labWorkHistoryService.getAllHistory();
+        model.addAttribute("historyList", historyList);
+        String adminName = authentication.getName();
+        model.addAttribute("username", adminName);
+        return "admin-history";
     }
 
     @PostMapping("/approve/{id}")
