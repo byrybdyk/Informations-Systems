17d365564ae22318be8f487b5d78916976978633
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2024-11-14 00:42:15 +0300

    feat: start user requests page

--- backend/src/main/java/com/byrybdyk/lb1/repository/PersonRepository.java
+++ backend/src/main/java/com/byrybdyk/lb1/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.byrybdyk.lb1.repository;
 import com.byrybdyk.lb1.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByName(String authorName);
 }
