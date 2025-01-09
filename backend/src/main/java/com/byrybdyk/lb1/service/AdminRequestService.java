dd8e55b4313e9134e35dab1389f953221b15439e
byrybdyk <90798897+byrybdyk@users.noreply.github.com>
2025-01-09 17:11:57 +0300

    feat: admins registration from keycloak API

--- backend/src/main/java/com/byrybdyk/lb1/service/AdminRequestService.java
+++ backend/src/main/java/com/byrybdyk/lb1/service/AdminRequestService.java
@@ -5,14 +5,43 @@ import com.byrybdyk.lb1.model.User;
 import com.byrybdyk.lb1.model.enums.Role;
 import com.byrybdyk.lb1.repository.AdminRequestRepository;
 import com.byrybdyk.lb1.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.ws.rs.core.Response;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.Map;
 
+
+import org.keycloak.representations.idm.UserRepresentation;
+
+
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class AdminRequestService {
+
+    @Value("${keycloak.auth-server-url}")
+    private String keycloakServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.resource}")
+    private String clientId;
+
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    @Value("${keycloak.admin.password}")
+    private String AdminPassword;
 
     private final AdminRequestRepository adminRequestRepository;
     private final UserRepository userRepository;
@@ -34,14 +63,84 @@ public class AdminRequestService {
         AdminRequest request = adminRequestRepository.findById(requestId)
                 .orElseThrow(() -> new RuntimeException("Request not found"));
 
-        User newUser = new User();
-        newUser.setUsername(request.getUsername());
-        newUser.setPassword(request.getPassword());
-        newUser.setRole(Role.ADMIN);
+        try {
+            // Создаем пользователя в Keycloak
+            createUserInKeycloak(request.getUsername(), request.getPassword(), "admin");
 
-        userRepository.save(newUser);
+            // Удаляем заявку после успешного создания пользователя
+            adminRequestRepository.delete(request);
 
-        adminRequestRepository.delete(request);
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при добавлении пользователя в Keycloak: " + e.getMessage(), e);
+        }
+    }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private void createUserInKeycloak(String username, String password, String role) {
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8180")
+                .realm("IS-realm")
+                .username("admin-user")
+                .password(AdminPassword)
+                .clientId("admin-cli")
+                .build();
+        System.out.println("Connected to Keycloak");
+
+        String realm = "IS-realm";
+
+        List<UserRepresentation> existingUsers = keycloak.realm(realm).users().search(username);
+        if (!existingUsers.isEmpty()) {
+            throw new RuntimeException("Пользователь с именем " + username + " уже существует");
+        }
+
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername(username);
+        user.setEnabled(true);
+        user.setAttributes(Map.of("requested_role", Collections.singletonList(role)));
+
+        CredentialRepresentation credentials = new CredentialRepresentation();
+        credentials.setType(CredentialRepresentation.PASSWORD);
+        credentials.setValue(password);
+        credentials.setTemporary(false);
+
+        user.setCredentials(Collections.singletonList(credentials));
+
+        Response response = keycloak.realm(realm).users().create(user);
+        if (response.getStatus() != 201) {
+            System.out.println("Ошибка при добавлении пользователя: " + response.getStatus() + " " + response.readEntity(String.class));
+            throw new RuntimeException("Ошибка при добавлении пользователя в Keycloak: " + response.getStatus());
+        }
+
+        System.out.println("User created in Keycloak");
+
+        List<RoleRepresentation> realmRoles = keycloak.realm(realm)
+                .roles()
+                .list();
+        RoleRepresentation adminRole = realmRoles.stream()
+                .filter(r -> r.getName().equals("ROLE_ADMIN"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ROLE_ADMIN " + role + " не найдена в Keycloak"));
+
+        System.out.println("Role found: " + adminRole.getName());
+
+        List<UserRepresentation> users = keycloak.realm(realm).users().search(username);
+        if (users.isEmpty()) {
+            throw new RuntimeException("Пользователь " + username + " не найден в Keycloak");
+        }
+        String userId = users.get(0).getId();
+
+        keycloak.realm(realm)
+                .users()
+                .get(userId)
+                .roles()
+                .realmLevel()
+                .add(Collections.singletonList(adminRole));
+
+        keycloak.realm(realm)
+                .users()
+                .get(userId)
+                .joinGroup("48075453-64ac-4f02-be02-64e3ae81b9f2");
+
+        System.out.println("User added to ADMIN-group ");
     }
 
     public List<AdminRequest> getAllRequests() {
--- backend/src/main/java/com/byrybdyk/lb1/service/KeycloakAdminClientService.java
+++ backend/src/main/java/com/byrybdyk/lb1/service/KeycloakAdminClientService.java
@@ -1,9 +1,11 @@
 package com.byrybdyk.lb1.service;
 
+import com.byrybdyk.lb1.model.AdminRequest;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.stereotype.Service;
@@ -26,27 +28,46 @@ public class KeycloakAdminClientService {
     @Value("${keycloak.credentials.secret}")
     private String clientSecret;
 
+    @Value("${keycloak.admin.password}")
+    private String AdminPassword;
+
+    @Autowired
+    AdminRequestService adminRequestRepository;
+
     public void registerUser(String username, String password, String role) throws Exception {
-        Keycloak keycloak = KeycloakBuilder.builder()
-                .serverUrl(keycloakServerUrl)
-                .realm("master")
-                .username("admin") // Укажите данные администратора
-                .password("admin-password")
-                .clientId("admin-cli")
-                .build();
+        try {
 
-        UserRepresentation user = new UserRepresentation();
-        user.setUsername(username);
-        user.setEnabled(false); // Отключаем, если роль ADMIN требует подтверждения
-        user.setAttributes(Map.of("requested_role", Collections.singletonList(role)));
+            if ("admin".equalsIgnoreCase(role)) {
+                adminRequestRepository.createRequest(username, password);
+                System.out.println("Admin request created");
+            } else {
+                Keycloak keycloak = KeycloakBuilder.builder()
+                        .serverUrl(keycloakServerUrl)
+                        .realm("IS-realm")
+                        .username("client_admin-cli")
+                        .password(AdminPassword)
+                        .clientId("IS-admin")
+                        .build();
 
-        CredentialRepresentation credential = new CredentialRepresentation();
-        credential.setType(CredentialRepresentation.PASSWORD);
-        credential.setValue(password);
-        credential.setTemporary(false);
+                UserRepresentation user = new UserRepresentation();
+                user.setUsername(username);
+                user.setEnabled(false);
+                user.setAttributes(Map.of("requested_role", Collections.singletonList(role)));
 
-        user.setCredentials(Collections.singletonList(credential));
+                CredentialRepresentation credential = new CredentialRepresentation();
+                credential.setType(CredentialRepresentation.PASSWORD);
+                credential.setValue(password);
+                credential.setTemporary(false);
 
-        keycloak.realm(realm).users().create(user);
+                user.setCredentials(Collections.singletonList(credential));
+
+                keycloak.realm(realm).users().create(user);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e + " ERROR");
+            throw new Exception("User already exists");
+        }
+
     }
 }
